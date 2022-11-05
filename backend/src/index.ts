939c07191b6f4c69d5d2e37c3ca2edb1f4b71fa0
Siddhant Sharma <sharmasiddhant301@gmail.com>
2022-11-05 14:22:57 -0700

    Add web3/ipfs integration

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { exec } from "child_process";
+import cors from "cors";
+import { Web3Storage, File, getFilesFromPath } from "web3.storage";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+var contractAddrToCID = new Map<string, any>();
+
+// @ts-ignore
+const storageClient = new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN })
+
+const app: express.Express = express();
+app.use(cors());
+
+app.get("/test", async (req: express.Request, res: express.Response) => {
+  const contractAddr = req.query.contractAddr;
+  if (contractAddrToCID.has(contractAddr as string)) {
+    const cid = contractAddrToCID.get(contractAddr as string);
+    return res.status(200).json({cid: cid});
+  }
+  try {
+    exec(`slither ${contractAddr} --json ${contractAddr}.json`);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+  const file = await getFilesFromPath(`${contractAddr}.json`)
+  const cid = storageClient.put(file);
+  contractAddrToCID.set(contractAddr as string, cid);
+  return res.status(200).json({cid: cid});
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
