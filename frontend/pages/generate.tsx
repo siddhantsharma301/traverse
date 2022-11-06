4f557ac38b9846f233c6c6213a05936813d9b328
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-11-05 20:36:45 -0700

    pushed input style

--- frontend/pages/generate.tsx
+++ frontend/pages/generate.tsx
@@ -1,44 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { WidgetProps } from '@worldcoin/id'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { WidgetProps } from "@worldcoin/id";
 import dynamic from "next/dynamic";
-import Link from 'next/link';
-
+import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
-    data: any[]
+  data: any[];
 }
 
 export default function Home({ data }: Props) {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Traverse</title>
-                <meta name="description" content="Smart contract security report generator" />
-                <link rel="icon" href="/favicon.png" />
-            </Head>
+  const [input, setInput] = useState<string>("");
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Traverse</title>
+        <meta
+          name="description"
+          content="Smart contract security report generator"
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title_recent}>
-                    Generate Security Report
-                </h1>
+      <main className={styles.main}>
+        <h1 className={styles.title_recent}>Generate Security Report</h1>
 
-            </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href=""
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Made with
-                    <span className={styles.logo}>
-                        <Image src="/heart.png" alt="Heart Logo" width={20} height={20} />
-                    </span>
-                    {' '}at ETHSF
-                </a>
-            </footer>
-        </div>
-    )
+        <input type={"text"} placeholder="Insert Contract Address" value={input} onChange={(e)=>setInput(e.target.value)} style={{}}/>
+      </main>
+    </div>
+  );
 }
