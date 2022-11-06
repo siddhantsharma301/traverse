2f1db77ac2ae5049f48a17d8646fd308079c3969
Siddhant Sharma <sharmasiddhant301@gmail.com>
2022-11-05 22:40:06 -0700

    Checkpoint for report creation

--- frontend/pages/contracts/[contract_address].tsx
+++ frontend/pages/contracts/[contract_address].tsx
@@ -15,6 +15,9 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
+import { Web3Storage } from "web3.storage";
+import * as dotenv from "dotenv";
+
 function createData(number: any, item: any, qty: any, price: any) {
     return { number, item, qty, price };
 }
@@ -33,9 +36,12 @@ const WorldIDWidget = dynamic<WidgetProps>(
     { ssr: false }
 )
 
-export default function Contracts() {
+export default function Contracts({stats}:{stats:any}) {
     const router = useRouter()
-    const { contract_address } = router.query
+    const { contractAddress } = router.query
+
+    // console.log(stats)
+
     return (
         <div className={styles.container}>
             <Head>
@@ -46,9 +52,8 @@ export default function Contracts() {
 
             <main className={styles.main}>
                 <h1 className={styles.title_recent}>
-                    Contract Address: {contract_address}
+                    Contract Address: {contractAddress}
                 </h1>
-
 
                 <WorldIDWidget
                     actionId="wid_staging_69e75b2d27bd76510d5752a719fde7e8" // obtain this from developer.worldcoin.org
@@ -59,7 +64,7 @@ export default function Contracts() {
                     debug={true} // to aid with debugging, remove in production
                 />
 
-                <TableContainer className={styles.table_container} component={Paper}>
+                {/* <TableContainer className={styles.table_container} component={Paper}>
                     <Table className={styles.table} aria-label="simple table">
                         <TableHead className={styles.table_head}>
                             <TableRow className={styles.table_row} >
@@ -82,10 +87,67 @@ export default function Contracts() {
                             ))}
                         </TableBody>
                     </Table>
+                </TableContainer> */}
+                <TableContainer className={styles.table_container} component={Paper}>
+                    <Table className={styles.table} aria-label="simple table">
+                        <TableHead className={styles.table_head}>
+                            <TableRow className={styles.table_row} >
+                                <TableCell className={styles.table_cell}>Impact</TableCell>
+                                <TableCell className={styles.table_cell} align="right">Confidence</TableCell>
+                                <TableCell className={styles.table_cell} align="right">Description</TableCell>
+                                <TableCell className={styles.table_cell} align="right">Check</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow className={styles.table_row} key={row.number}>
+                                    <TableCell className={styles.table_cell} component="th" scope="row">
+                                        {row.number}
+                                    </TableCell>
+                                    <TableCell className={styles.table_cell} align="right">{row.item}</TableCell>
+                                    <TableCell className={styles.table_cell} align="right">{row.qty}</TableCell>
+                                    <TableCell className={styles.table_cell} align="right">{row.price}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
                 </TableContainer>
 
             </main>
         </div>
 
     )
+}
+
+export const getServerSideProps = async (context: any) => {
+    const { contract_address } = context.params;
+    console.log(contract_address)
+    const scannerRes = await axios.get(`http://127.0.0.1:3000/test?contractAddr=${contract_address}`)
+    // @ts-ignore
+    const { cid } = scannerRes.data
+    // @ts-ignore
+    const client = new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN });
+    const response = await client.get(cid);
+    // @ts-ignore
+    if (!response.ok) {
+        throw new Error("Unable to fetch given CID");
+    }
+    // @ts-ignore
+    const file = await response.files();
+    const dataUnparsed = await file[0].text();
+    const data = JSON.parse(dataUnparsed);
+    const detections = data.results.detectors;
+
+    const rows: any[] = [];
+    detections.forEach((detection: any) => {
+        const obj = {
+            description: detection.description,
+            impact: detection.impact,
+            confidence: detection.confidence,
+            check: detection.check,
+        }
+        rows.push(obj);
+    });
+
+    return { props: { stats: rows } };
 }
